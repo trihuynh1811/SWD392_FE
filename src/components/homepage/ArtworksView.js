0934e40ce2b519772a2250be08a7837fb2b620ba
VantCii <viet12092003@gmail.com>
2024-03-18 02:34:26 +0700

    test artwork image

--- src/components/homepage/ArtworksView.js
+++ src/components/homepage/ArtworksView.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import av1 from '../../image/Homepage/artworkview_1.png'
+import av2 from '../../image/Homepage/artworkview_2.png'
+import av3 from '../../image/Homepage/artworkview_3.png'
+import av4 from '../../image/Homepage/artworkview_4.png'
 
 export const ArtworksView = () => {
   return (
-    <div>
-      
+    <div className='artworkview_container'>
+      <div className='artwork_img'>
+        <div>
+          <img src={av1} alt="" />
+        </div>
+        <div>
+          <img src={av2} alt="" />
+        </div>
+        <div>
+          <img src={av3} alt="" />
+        </div>
+        <div>
+          <img src={av4} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
