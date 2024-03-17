a66858339d664f32388de5217f83c7d9b6c9af4f
VantCii <viet12092003@gmail.com>
2024-03-18 02:05:17 +0700

    done discord component

--- src/components/homepage/Discord.js
+++ src/components/homepage/Discord.js
@@ -4,16 +4,24 @@ import discord_icon from '../../image/Icon/discord_icon.png'
 
 export const Discord = () => {
   return (
-    <div>
+    <div className='mt-[112px]'>
       <div className='relative'>
         <img className='w-full' src={discord_poster} alt="" />
-        <div className='absolute left-[2%] bottom-1'>
-          <h2>Be part of a <span>creative</span> community! <span>🌎</span></h2>
-          <button className='flex items-center'>
-            <div>
-              <img src={discord_icon} alt="" />
+        <div className='absolute left-[100px] bottom-[22%]'>
+          <h2 className='text-[56px] w-[709px] font-bold text-[#3D4449] leading-[80px]'>Be part of a <span className='text-[#FF6C79]'>creative</span> community!<span>🌎</span></h2>
+          <button className='flex 
+            items-center 
+            gap-[15px] 
+            bg-[#FF6C79]
+            px-[30px]
+            py-[14px]
+            rounded-[15px]
+            mt-[20px]'>
+            <div className='flex
+            items-center'>
+              <img src={discord_icon} className='w-[30px]' alt="" />
             </div>
-            <p>Join Discord Server</p>
+            <p className='text-[18px] text-[#FAFAFA]'>Join Discord Server</p>
           </button>
         </div>
       </div>
