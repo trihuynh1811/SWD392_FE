ea57246afe000902e293c38e0acd7491875b4cb5
VantCii <viet12092003@gmail.com>
2024-03-19 08:59:58 +0700

    TypeImageData for Type Hub

---
+++ src/components/typeCarousel/ImageTypeJSON.js
@@ -0,0 +1,38 @@
+export const TypeImageData =
+  [
+    {
+      "id": 1,
+      "type_name": "Landscapes",
+      "typeImage_url": "https://unsplash.com/photos/green-and-brown-mountains-under-white-clouds-during-daytime-gUDNK8NqYHk"
+    },
+    {
+      "id": 2,
+      "type_name": "Portrait",
+      "typeImage_url": "https://unsplash.com/photos/boy-in-brown-hat-painting-rBVcss6bjBA",
+    },
+    {
+      "id": 3,
+      "type_name": "Still life",
+      "typeImage_url": "https://unsplash.com/photos/a-painting-of-a-vase-with-a-plant-in-it-gbMoearkavc",
+    },
+    {
+      "id": 4,
+      "type_name": "Abstract",
+      "typeImage_url": "https://unsplash.com/photos/abstract-art-painting-eVaxJVA2zHI",
+    },
+    {
+      "id": 5,
+      "type_name": "Illustration",
+      "typeImage_url": "https://unsplash.com/photos/white-and-pink-rose-bouquet-ayNT6hDhiBw",
+    },
+    {
+      "id": 6,
+      "type_name": "Animals",
+      "typeImage_url": "https://unsplash.com/photos/orange-and-white-tabby-cat-lying-on-brown-and-black-floral-textile-Wiad3DQxUho",
+    },
+    {
+      "id": 7,
+      "type_name": "Genres",
+      "typeImage_url": "https://unsplash.com/photos/yellow-and-purple-flowers-with-green-leaves-mJh7mv4Ni2E",
+    }
+  ]
